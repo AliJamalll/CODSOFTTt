7c76dd6dbf371cac7a8d58dedd69919b69d5b3f9
Ali Jamal <you@example.com>
2024-06-10 00:05:38 +0300

    first

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:chatapp/pages/login_page.dart';
+import 'package:chatapp/pages/register_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {/// to connect the string with it's value
+        "LogInPage" : (context) => LogInPage(),
+        RegisterScreen.id : (context) => RegisterScreen(),
+      },
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: "LogInPage",
+    );
+  }
+}
+
